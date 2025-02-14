577ef606743b3390cde4581d8d900d6f2365d07b
Tigas <treisalmeida@gmail.com>
2025-02-14 09:32:41 -0300

    ultimo commit back-end

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 user = "postgres"
 password = "tiagoreis123"
@@ -9,11 +10,18 @@ host = "localhost"
 SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
+SessionLocal = sessionmaker( autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 try:
     with engine.connect() as conn:
         print("Conexão bem-sucedida!")
 except Exception as e:
     print(f"Erro ao conectar ao banco: {e}")
 
-SessionLocal = sessionmaker( autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from fastapi.params import Body
-import classes
-
-from database import engine
+from fastapi import Depends
+from fastapi import status
+from typing import List
+from sqlalchemy.orm import Session
+from database import get_db
 from model import Base
+from database import engine
+import model
+from fastapi.middleware.cors import CORSMiddleware
+
+
+import classes
 
 Base.metadata.create_all(bind=engine)
 print("Tabelas criadas com sucesso!")
@@ -11,26 +19,50 @@ print("Tabelas criadas com sucesso!")
 
 app = FastAPI()
 
-    
-@app.get ("/")
+origins = [
+    'http://localhost:3000'
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+
+@app.get("/")
 def read_root():
     return {"Hello": "lala"}
 
-@app.post("/criar")
-def criar_valores(nova_mensagem: classes.Mensagem):
-    print(nova_mensagem)
-    return {"Mensagem": f"Titulo: {nova_mensagem.titulo} Mensagem: {nova_mensagem.conteudo} Publicada: {nova_mensagem.publicada}"}
 
-@app.post("/criar")
-def criar_valores(res: dict = Body(...)):
-    print(res)
-    return {"Mensagem": "Criado com Sucesso!"}
+@app.post("/criar", status_code=status.HTTP_201_CREATED)
+def criar_valores(nova_mensagem: classes.Mensagem, db: Session = Depends(get_db)):
+    mensagem_criada = model.Model_Mensagem(titulo=nova_mensagem.titulo,
+                                           conteudo=nova_mensagem.conteudo, publicada=nova_mensagem.publicada)
+    mensagem_criada = model.Model_Mensagem(**nova_mensagem.model_dump())
+    db.add(mensagem_criada)
+    db.commit()
+    db.refresh(mensagem_criada)
+    return {"Mensagem": mensagem_criada}
 
-@app.post("/criar")
-def criar_valores(res: dict = Body(...)):
-    print(res)
-    return {"Mensagem": f"lala: {res['lala']} lele: {res['lele']}"}
+# @app.post("/criar")
+# def criar_valores(res: dict = Body(...)):
+#     print(res)
+#     return {"Mensagem": "Criado com Sucesso!"}
+
+# @app.post("/criar")
+# def criar_valores(res: dict = Body(...)):
+#     print(res)
+#     return {"Mensagem": f"lala: {res['lala']} lele: {res['lele']}"}
+
 
 @app.get("/quadrado/{num}")
 def square(num: int):
- return num ** 2
+    return num ** 2
+
+
+@app.get("/mensagens", response_model=List[classes.Mensagem], status_code=status.HTTP_200_OK)
+async def buscar_valores(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
+    mensagens = db.query(model.Model_Mensagem).offset(skip).limit(limit).all()
+    return mensagens
