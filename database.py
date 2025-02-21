95096dfc18703405ebfb474c09e7efcf671ec15a
Tigas <treisalmeida@gmail.com>
2025-02-21 11:26:25 -0300

    yml

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
-# Carregar variáveis do .env
-load_dotenv()
-
+if os.getenv("GITHUB_ACTIONS") is None:
+    load_dotenv(".env.local")
+    
 # Ler variáveis de ambiente com fallback
 user = "postgres"
 password = "tiagoreis123"
@@ -18,11 +18,12 @@ print(f"DB_PASSWORD={'*' * len(password) if password else None}")
 print(f"DB_NAME={database}")
 print(f"DB_HOST={host}")
 
-# Construir a URL de conexão
 SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
 
-# Criar engine
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+# Log para verificar a URL de conexão
+print(f"Database URL: {SQLALCHEMY_DATABASE_URL}")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
@@ -32,8 +33,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
-# Testar conexão
 try:
     with engine.connect() as conn:
         print("Conexão bem-sucedida!")
